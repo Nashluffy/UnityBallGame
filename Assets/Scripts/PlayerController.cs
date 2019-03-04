55ce2dd9ce078eff6baa1aeef7224c6929ad76c2
Nash Luffman <nashluffman@gmail.com>
2019-03-04 11:40:58 -0500

    Initial commit, 5 of 8

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    public float gravity;
+    // Update is called once per frame
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+    }
+    void FixedUpdate() 
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveZed = Input.GetAxis("Jump");
+        Vector3 movement = new Vector3 (moveHorizontal, moveZed * (gravity/speed), moveVertical);
+        rb.AddForce (movement * speed);
+        
+    }
+}
